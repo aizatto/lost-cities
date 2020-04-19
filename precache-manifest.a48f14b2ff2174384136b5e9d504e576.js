6dc4c3d569bf5408097d7f225d1dfa692cf843ae
Aizat Faiz <aizat.faiz@gmail.com>
2020-04-19 09:46:26 +0800

    Updates

---
+++ precache-manifest.a48f14b2ff2174384136b5e9d504e576.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cba0dc2693ae7fb69ad327168e992cc8",
+    "url": "/lost-cities/index.html"
+  },
+  {
+    "revision": "ecab82bbd3a57cc413cd",
+    "url": "/lost-cities/static/css/2.a43b59a1.chunk.css"
+  },
+  {
+    "revision": "518c8adce96f92cb4fa4",
+    "url": "/lost-cities/static/css/main.27fd15f6.chunk.css"
+  },
+  {
+    "revision": "ecab82bbd3a57cc413cd",
+    "url": "/lost-cities/static/js/2.8f0c0bdc.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/lost-cities/static/js/2.8f0c0bdc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "518c8adce96f92cb4fa4",
+    "url": "/lost-cities/static/js/main.6fe92da5.chunk.js"
+  },
+  {
+    "revision": "1ce89ae355b0a17c6f88",
+    "url": "/lost-cities/static/js/runtime-main.95969cc5.js"
+  }
+]);
